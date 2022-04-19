5fa8249f7939657585398833043cbeb8edb51c6e
JulSarmiento <jasa1999@hotmail.com>
2022-04-19 11:21:12 -0500

    [Coder House] feat: working in the cart controller

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -7,22 +7,27 @@ const cart = new Container(db);
 
 // add new cart of products
 exports.save = async function(req, res) {
-    console.log(req.body)
+  console.log(req.body)
+
+  res.json(await cart.save(req.body));
 }
 
-// save an item in an existen cart (like an update)
+// save an item in an existen cart (like an update) PENDIENTE
 exports.saveCartItem = async function(req, res) {
+
+  console.log('body',req.body);
 
 }
 
 // return one cart by its id
 exports.getById = async function(req, res){
-  return res.json(req.cart);
+  return res.json(req.body.products);
 }
 
 // delete all products in the cart
-exports.deleteAll = async function(req, res) {
-  res.json(await cart.deleteAll());
+exports.deleteById = async function(req, res){
+  const {id} = req.params;  
+  res.json(await cart.deleteById(id));
 }
 
 // delete an existen  product in the cart by its id 
